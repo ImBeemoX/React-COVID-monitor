82957e6d83b5d1bc6baa36f3f5aa35b1a3bac987
Yichuan Wang <w369371@gmail.com>
2020-04-17 03:03:58 +1000

    change list to table, add new confirmed and total confirmed

--- src/Components/Lists.js
+++ src/Components/Lists.js
@@ -1,27 +1,44 @@
-import React, { useState } from "react";
-import ListGroup from "react-bootstrap/ListGroup";
+import React, { useState, useEffect } from "react";
 import getData from "../Hooks/getData";
+import Table from "react-bootstrap/Table";
 
 const Lists = () => {
-  const a = [1, 2, 3, 4, 5];
   const [countries, setCountries] = useState([]);
 
-  if (countries.length == 0) {
+  if (countries.length === 0) {
     getData().then((data) => {
       setCountries(data.data.Countries);
-      console.log(countries);
     });
   }
 
-  const countryList = countries.map((country) => {
-    return (
-      <ListGroup.Item eventKey={country.CountryCode}>
-        {country.Country}
-      </ListGroup.Item>
-    );
+  useEffect(() => {
+    console.log(countries.length);
   });
 
-  return <ListGroup>{countryList}</ListGroup>;
+  const getInfo = () => {
+    return countries.map((country) => {
+      return (
+        <tr>
+          <td>{country.Country}</td>
+          <td>{country.NewConfirmed}</td>
+          <td>{country.TotalConfirmed}</td>
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Country Name</th>
+          <th>New Confirmed</th>
+          <th>Total Confirmed</th>
+        </tr>
+      </thead>
+      <tbody>{getInfo()}</tbody>
+    </Table>
+  );
 };
 
 export default Lists;
--- src/Hooks/getData.js
+++ src/Hooks/getData.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { useState } from "react";
 
 const getInfo = async () => {
   let temp = await axios.get("https://api.covid19api.com/summary");
